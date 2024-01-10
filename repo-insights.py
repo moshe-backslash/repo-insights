cd8f984c0709a5eef2096efb72776855d8aca798
moshe-backslash <moshe.cohen@backslash.security>
2024-01-10 18:21:35 +0200

    WIP

--- repo-insights.py
+++ repo-insights.py
@@ -1,49 +1,54 @@
-from github import Github
+import argparse
+from github import Github, Auth
 import pprint
 
-# Authentication is defined via github.Auth
-from github import Auth
-
-
-def x(file_content):
+def x(file_content, repo):
     if file_content.type == "dir":
         print("DIR", file_content.path)
         contents = repo.get_contents(file_content.path)
         for fc in contents:
-            x(fc)
+            x(fc, repo)
     else:
         print("\t", file_content.path)
 
 
+def generate_insights(repo_path, token):
+    auth = Auth.Token(token)
+    g = Github(auth=auth)
+    print(55,g, repo_path, token)
 
-# using an access token
-auth = Auth.Token("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")
+    repo = g.get_repo(repo_path)
+    print(repo.name)
 
-# First create a Github instance:
-
-# Public Web Github
-g = Github(auth=auth)
-print(55,g)
-
-
-# Then play with your Github objects:
-u= g.get_user()
-print(99, u)
-for repo in g.get_user().get_repos():
-     print(repo.name)
-
-repo = g.get_repo("mosheco/appster2")
-print(repo.name)
-
-for i in range(1000000):
-    print (i)
     contents = repo.get_contents("")
     print(contents)
 
-for file_content in   contents:
-    x(file_content)
+    for file_content in   contents:
+        x(file_content, repo)
+
+    g.close()
 
 
-# To close connections after use
-g.close()
+def get_parser():
+    """
+    Parse the command-line arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--repo',
+                        help="full path of repo (<user>/<name>)",
+                        dest='repo',
+                        required=True)
+
+    parser.add_argument('--token',
+                        help="an access token provided by Github",
+                        default="token",
+                        required=True)
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    generate_insights(args.repo, args.token)
 
