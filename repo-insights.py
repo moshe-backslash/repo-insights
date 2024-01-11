332c7fff233a9c7d802bf1538d98f9057c6999e5
moshe-backslash <moshe.cohen@backslash.security>
2024-01-11 14:25:51 +0200

    WIP

--- repo-insights.py
+++ repo-insights.py
@@ -1,30 +1,40 @@
 import argparse
+import base64
 from github import Github, Auth
+import pandas as pd
+import pathlib
 import pprint
 
-def x(file_content, repo):
-    if file_content.type == "dir":
-        print("DIR", file_content.path)
-        contents = repo.get_contents(file_content.path)
-        for fc in contents:
-            x(fc, repo)
-    else:
-        print("\t", file_content.path)
+
+def get_all_file_names(repo):
+    files = []
+    contents = repo.get_contents("")
+    while contents:
+        file_content = contents.pop(0)
+        if file_content.type == "dir":
+            contents.extend(repo.get_contents(file_content.path))
+        else:
+            line_count = base64.b64decode(file_content.content).decode().count('\n')
+            suffix = pathlib.Path(file_content.name).suffix
+            files.append((file_content.name, suffix, line_count))
+    return files
+
+
+def suffix_based_stats(file_tuple_list):
+    df = pd.DataFrame(data=file_tuple_list, columns=['name', 'suffix', 'line_count'])
+    stats = df.groupby(['suffix'])['line_count'].agg(['sum', 'count']).sort_values('count', ascending=False)
+    print(stats)
 
 
 def generate_insights(repo_path, token):
     auth = Auth.Token(token)
     g = Github(auth=auth)
-    print(55,g, repo_path, token)
-
     repo = g.get_repo(repo_path)
-    print(repo.name)
 
-    contents = repo.get_contents("")
-    print(contents)
+    file_tuple_list = get_all_file_names(repo)
+    pprint.pprint(file_tuple_list)
 
-    for file_content in   contents:
-        x(file_content, repo)
+    suffix_based_stats(file_tuple_list)
 
     g.close()
 
@@ -51,4 +61,3 @@ if __name__ == '__main__':
     parser = get_parser()
     args = parser.parse_args()
     generate_insights(args.repo, args.token)
-
